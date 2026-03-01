00d3712db230e3462a57e891128b442714787264
Daniel Lang <dkllang1@gmail.com>
2026-03-01 11:06:53 -0600

    updating changelog and incrementing to version 1.7.0

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ const IS_DEV = process.env.APP_VARIANT === 'development';
 export default {
   name: IS_DEV ? 'U-Stat (Dev)' : 'U-Stat',
   slug: 'ultimatestats',
-  version: '1.6.0',
+  version: '1.7.0',
   orientation: 'default',
   icon: './assets/images/icon.png',
   scheme: IS_DEV ? 'ultimatestats-dev' : 'ultimatestats',
